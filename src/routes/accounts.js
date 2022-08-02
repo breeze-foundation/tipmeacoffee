81298aa7dbeeec561645279a855a9829c0ecb996
NickBird <almabroral@gmail.com>
2022-08-02 10:46:00 +0500

    Update accounts.js

--- src/routes/accounts.js
+++ src/routes/accounts.js
@@ -41,7 +41,6 @@ async function signup(req, res) {
         let post = req.body; 
         let uEmail = escape(req.body.email);
         let checkEmail = await isFakeEmailOnline(uEmail)
-        console.log(checkEmail.isFakeDomain)
         console.log(req.clientIp + 'email address is ' + uEmail)
         if(checkEmail.isFakeDomain !== false){res.send({ error: true, message: 'Email address not allowed' }); return false }
         let uName = post.name.toLowerCase(); 
